5f89545e781c79c932019a81f81fcd72a638502b
Tlntin <TlntinDeng01@Gmail.com>
2021-11-20 16:21:20 +0800

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
+## Raw README
 # Super-Mario-Bros
+
 Remake of the original Super Mario Bros game.
 
 Here's the video explaining how I did it: https://youtu.be/7D4uoSoQsjw
+
+## NewREADME
+### My modification
+1. Screen widening
+2. Added background sound effects, death sound effects, and mushroom picking sound effects.
+3. Increase the compatibility of MacOS, (by default, you can only use the keyboard when you open it in the terminal, and you can run it directly in xcode after modification and use the keys)
+4. Press the space bar to automatically advance, similar to parkour mode, press again to stop.
+5. After death, it will be automatically resurrected after 3 seconds, and the monsters that have been eliminated will not be refreshed, which reduces the difficulty of the game. 
+
+### Known bug
+1. Under MacOS, you cannot press two arrow keys at the same time, such as pressing "Right" and "Up". By default, only "Up" will be activated.
+
+    Solution 1.: Press the space bar to enter the parkour mode (that is, auto forward or auto backward, so that only one arrow key is needed each time), to cancel the parkour mode, you can press the arrow key again.
+
+    Solution 2: Click mario.app, right-click "show package content", find the MacOS folder, double-click to open the shell binary inside, then go to settings, privacy settings, select input monitoring, check "terminal.app", and then click You can use the double arrow keys.
+
+2. The game will not end after clearing, wait for the original author to update.
+
+### Game buttons
+1. Arrow keys, up, down, left, and right.
+2. `Z`: Can replace the arrow keys to achieve jump.
+3. `Esc` to exit the game.
+4. `Enter` to reset the game.
+5. `Space` parkour mode, automatic forward or backward, controlled by the arrow keys. Press again to cancel.
+
+### Game screenshots
+
+![](Source/Images/screen.png)
+
+
+## 新README
+
+### 我的修改
+
+1. 屏幕拉宽
+2. 增加背景音效，死亡音效，采蘑菇音效。
+3. 增加MacOS的兼容性，（默认只能在终端打开才能用键盘，修改后可以直接在xcode运行，且使用按键）
+4. 按下空格键自动前进，类似跑酷模式，再按一次可以停止。
+5. 死亡后，3秒后自动复活，已经消灭的怪物不会刷新，降低游戏难度。
+
+### 已知bug
+
+1. MacOS下不能同时按两个方向键，比如按了"右"，"上"，默认只会激活"上"。
+
+   解决方法1. ：按下空格键进入跑酷模式（也就是自动前进或者自动后退，这样每次只需要一个方向键就行了），取消跑酷模式可以再按一次方向键。
+
+   解决方法2：点击mario.app，右键"显示包内容"，找到MacOS文件夹，双击打开里面的shell二进制，然后去设置，隐私设置里面，选择输入监控，勾选"终端.app"，然后就可以用双方向键了。
+
+2. 通关后游戏不会结束，等原作者更新。
+
+### 游戏按键
+
+1. 方向键，上下左右。
+2. `Z`: 可代替方向键上，实现跳跃。
+3. `Esc`退出游戏。
+4. `Enter`重置游戏。
+5. `Space`跑酷模式，自动前进或者后退，由方向键控制。再按一次取消。
+
+### 游戏截图
+
+![](Source/Images/screen.png)
--- Source/Mario.cpp
+++ Source/Mario.cpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <chrono>
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 #include "Headers/Animation.hpp"
@@ -18,9 +19,9 @@ Mario::Mario() :
 	y(0),
 	jump_timer(0),
 	death_timer(MARIO_DEATH_DURATION),
-	walk_animation(MARIO_WALK_ANIMATION_SPEED, CELL_SIZE, "Resources/Images/MarioWalk.png")
+	walk_animation(MARIO_WALK_ANIMATION_SPEED, CELL_SIZE, resourcePath() + "Images/MarioWalk.png")
 {
-	texture.loadFromFile("Resources/Images/MarioIdle.png");
+	texture.loadFromFile(resourcePath() + "Images/MarioIdle.png");
 
 	sprite.setTexture(texture);
 }
@@ -28,6 +29,13 @@ Mario::Mario() :
 bool Mario::get_dead() const
 {
 	return dead;
+}
+
+unsigned char Mario::get_jump_timer(){
+    return this->jump_timer;
+}
+void Mario::set_jump_timer(unsigned char i_value){
+    this->jump_timer = i_value;
 }
 
 float Mario::get_vertical_speed() const
@@ -44,7 +52,30 @@ void Mario::die()
 {
 	dead = 1;
 
-	texture.loadFromFile("Resources/Images/MarioDeath.png");
+	texture.loadFromFile(resourcePath() + "Images/MarioDeath.png");
+    // Load a music to play
+    if (!music.openFromFile(resourcePath() + "audio/die.wav")) {
+        return EXIT_FAILURE;
+    }
+//    std::cout << "died" << std::endl;
+    music.setLoop(false);
+    music.play();
+}
+
+void Mario::start_music(){
+    if (!music.openFromFile(resourcePath() + "audio/bgm.wav")) {
+        return EXIT_FAILURE;
+    }
+    music.setLoop(true);
+    music.play();
+}
+
+void Mario::start_other_music(std::string path){
+    if (!other_music.openFromFile(path)) {
+        return EXIT_FAILURE;
+    }
+    other_music.setLoop(false);
+    other_music.play();
 }
 
 void Mario::draw(sf::RenderWindow& i_window)
@@ -57,13 +88,13 @@ void Mario::draw(sf::RenderWindow& i_window)
 	{
 		if (0 == on_ground)
 		{
-			texture.loadFromFile("Resources/Images/MarioJump.png");
+			texture.loadFromFile(resourcePath() + "Images/MarioJump.png");
 		}
 		else
 		{
 			if (0 == horizontal_speed)
 			{
-				texture.loadFromFile("Resources/Images/MarioIdle.png");
+				texture.loadFromFile(resourcePath() + "Images/MarioIdle.png");
 			}
 			//You have no idea how many hours I spent trying to figure this out.
 			else if ((0 < horizontal_speed && 0 == sf::Keyboard::isKeyPressed(sf::Keyboard::Right) &&
@@ -72,7 +103,7 @@ void Mario::draw(sf::RenderWindow& i_window)
 					  1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Right)))
 			
 			{
-				texture.loadFromFile("Resources/Images/MarioBrake.png");
+				texture.loadFromFile(resourcePath() + "Images/MarioBrake.png");
 			}
 			else
 			{
@@ -111,10 +142,10 @@ void Mario::reset()
 	y = 0;
 
 	jump_timer = 0;
-
+    this->start_music();
 	death_timer = MARIO_DEATH_DURATION;
 
-	texture.loadFromFile("Resources/Images/MarioIdle.png");
+	texture.loadFromFile(resourcePath() + "Images/MarioIdle.png");
 
 	sprite.setTexture(texture);
 
@@ -133,32 +164,60 @@ void Mario::set_vertical_speed(float i_value)
 	vertical_speed = i_value;
 }
 
-void Mario::update(const Map& i_map)
+void Mario::set_horizontal_speed(float i_value)
 {
+    horizontal_speed = i_value;
+}
+
+void Mario::update(const Map& i_map, const sf::Event & event, bool moving)
+{
+//    bool moving = 1;
+    bool move_left = 0;
+    bool move_right = 0;
+    bool move_jump = 0;
 	if (0 == dead)
 	{
-		bool moving = 0;
 
 		unsigned char horizontal_collision;
 		unsigned char vertical_collision;
 
-		on_ground = 0;
-
+		on_ground = 1;
+        // move left
 		if (0 == sf::Keyboard::isKeyPressed(sf::Keyboard::Right) &&
 			1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            move_left = 1;
+        }
+        // macOS compatiable
+        else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left)
 		{
+            move_left = 1;
+        }
+        if (move_left) {
 			moving = 1;
-
 			horizontal_speed = std::max(horizontal_speed - MARIO_ACCELERATION, -MARIO_WALK_SPEED);
+            last_horizontal_speed = horizontal_speed;
 		}
-
+        // move right
+        
 		if (0 == sf::Keyboard::isKeyPressed(sf::Keyboard::Left) &&
 			1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            move_right = 1;
+        }
+        // macOS compatiable
+        else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
+        {
+            move_right = 1;
+        }
+        if (move_right)
 		{
 			moving = 1;
 
 			horizontal_speed = std::min(MARIO_ACCELERATION + horizontal_speed, MARIO_WALK_SPEED);
+            last_horizontal_speed = horizontal_speed;
 		}
+
 
 		if (0 == moving)
 		{
@@ -182,6 +241,7 @@ void Mario::update(const Map& i_map)
 		//{bottom-right collision, bottom-left collision, top-right collision, top-left collision}
 
 		horizontal_collision = map_collision(horizontal_speed + x, y, {Cell::Brick, Cell::Pipe, Cell::QuestionBlock, Cell::Wall}, i_map);
+//        std::cout << horizontal_collision << "\t speed: " << horizontal_speed << std::endl;
 
 		if (0 < horizontal_collision)
 		{
@@ -208,10 +268,24 @@ void Mario::update(const Map& i_map)
 
 		//Is Mario standing on the ground?
 		vertical_collision = map_collision(x, 1 + y, {Cell::Brick, Cell::Pipe, Cell::QuestionBlock, Cell::Wall}, i_map);
-		
+//        std::cout << "first: " << vertical_collision << std::endl;
 		//I added the <Z> key because some people will say, "I LiKe tO PrEsS ThE <Z> KeY InStEaD Of tHe <Up> KeY!"
-		if (1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || 1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+		if (1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Up) ||
+            1 == sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+        {
+            move_jump = 1;
+        } else if  (event.type == sf::Event::KeyPressed &&
+            (event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::Z))
+        {
+            move_jump = 1;
+        }
+        if (move_jump)
 		{
+//            horizontal_speed = 2;
+//            if (horizontal_speed == 0) {
+//                horizontal_speed = last_horizontal_speed * 2;
+//                last_horizontal_speed /= 2;
+//            }
 			if (0 == vertical_speed && 0 < vertical_collision)
 			{
 				vertical_speed = MARIO_JUMP_SPEED;
@@ -237,7 +311,8 @@ void Mario::update(const Map& i_map)
 		}
 
 		vertical_collision = map_collision(x, vertical_speed + y, {Cell::Brick, Cell::Pipe, Cell::QuestionBlock, Cell::Wall}, i_map);
-		
+//        std::cout << "horontal speed: " << horizontal_speed << std::endl;
+//        std::cout << "vetical speed: " << vertical_speed << std::endl;
 		if (0 < vertical_collision)
 		{
 			//3  - 0011 - top collision
@@ -298,9 +373,11 @@ void Mario::update(const Map& i_map)
 			y += vertical_speed;
 		}
 		else if (1 == death_timer)
-		{
+        {
 			vertical_speed = MARIO_JUMP_SPEED;
+            this->reset();
 		}
+        
 
 		death_timer = std::max(0, death_timer - 1);
 	}
